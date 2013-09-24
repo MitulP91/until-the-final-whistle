280704661d4cdcc74c2f170622054dfa4c641578
MitulP91 <mitulp91@gmail.com>
2013-09-24 18:51:54 -0400

    Heroku test

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,6 +3,14 @@ class TeamsController < ApplicationController
 
 	def index
 		@teams = Team.all
+
+		url = "http://api.statsfc.com/premier-league/fixtures.json?key=DThzCPsM_TI0XUGeUOJqr26JHwtYXVIfYvSSb0ui&timezone=America/New_York&limit=5"
+		response = HTTParty.get(url)
+		@data = response.parsed_response
+
+		url2 = "http://api.statsfc.com/top-scorers.json?key=DThzCPsM_TI0XUGeUOJqr26JHwtYXVIfYvSSb0ui&competition=premier-league&year=2013/2014&limit=5"
+		response2 = HTTParty.get(url2)
+		@data2 = response2.parsed_response		
 	end
 
 	def new
@@ -24,9 +32,13 @@ class TeamsController < ApplicationController
 		data = response.parsed_response
 		@articles = data["rss"]["channel"]["item"]
 
-		url2 = "http://api.statsfc.com/premier-league/fixtures.json?key=DThzCPsM_TI0XUGeUOJqr26JHwtYXVIfYvSSb0ui&team=#{query.downcase.gsub(/\s/, '+')}&timezone=America/New_York&limit=5"
+		url2 = "http://api.statsfc.com/premier-league/fixtures.json?key=DThzCPsM_TI0XUGeUOJqr26JHwtYXVIfYvSSb0ui&team=#{query.downcase.gsub(/\s/, '-')}&timezone=America/New_York&limit=5"
 		response2 = HTTParty.get(url2)
 		@data2 = response2.parsed_response
+
+		url3 = "http://api.statsfc.com/premier-league/results.json?key=DThzCPsM_TI0XUGeUOJqr26JHwtYXVIfYvSSb0ui&team=#{query.downcase.gsub(/\s/, '-')}&timezone=America/New_York&limit=5"
+		response3 = HTTParty.get(url3)
+		@data3 = response3.parsed_response
 	end
 
 	def edit
